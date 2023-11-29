9f5d86b170b5d9551e6fce3b55e2d4beb66ba697
lexapech <lexa.pech@yandex.ru>
2023-11-29 22:55:14 +0300

    added comments requests

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -99,4 +99,44 @@ export class AppController {
     console.log(changes)
     return {}
   }
+
+  @Post("/project/:id/comment")
+  async addComment(@Body() comment): Promise<any> {
+    console.log(comment.text)
+    return {date: new Date().toISOString(),text:comment.text}
+  }
+  @Get("/project/:id/comments")
+  async getComments(): Promise<any> {
+
+    return {comments:[{date:new Date(),text:"Because you can return or throw responses in loaders and actions, you can use redirect to redirect to another route.\n" +
+            "\n" +
+            "import { redirect } from \"react-router-dom\";\n" +
+            "\n" +
+            "const loader = async () => {\n" +
+            "  const user = await getUser();\n" +
+            "  if (!user) {\n" +
+            "    return redirect(\"/login\");\n" +
+            "  }\n" +
+            "  return null;\n" +
+            "};\n" +
+            "It's really just a shortcut for this:\n" +
+            "\n" +
+            "new Response(\"\", {\n" +
+            "  status: 302,\n" +
+            "  headers: {\n" +
+            "    Location: someUrl,\n" +
+            "  },\n" +
+            "});\n" +
+            "It's recommended to use redirect in loaders and actions rather than useNavigate in your components when the redirect is in response to data.\n" +
+            "\n" +
+            "See also:"},
+        {date:new Date(),text:"234"},
+        {
+          date:new Date(),text:"Что нужно заложить под натяжной потолок, чтобы после его не пришлось снимать? В моих ремонтах вы часто видите натяжные потолки, на которые установлены светильники, гардины и даже рельсы шкафа-купе, а потолки при этом не прогибаются и на них не образуются складки. Сегодня расскажу, как мы это делаем:)"
+        },
+        {
+          date:new Date(),text:"1) Закладные под декоративные перегородки. Как правило, мы добавляем эти закладные, если у заказчика разнополые дети и нам через несколько лет нужно будет разделить детскую комнату на две зоны. Как правило, для этих целей мы используем лёгкие ажурные конструкции, которые не перекрывают световой поток, но при этом зонируют комнату. При этом, мы заранее определяем габариты и сечение будущих перегородок, чтобы установить закладные соответствующего размера. "
+        }
+      ]}
+  }
 }
